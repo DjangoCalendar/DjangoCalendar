f65d204e7a5fd3a29712f76f3bb5db6e0bb5b6c0
bonzai1990 <mateusz.bacal90@o2.pl>
2014-02-19 19:59:26 +0100

    Watek GG
    raz odpalony odpala sie codziennie o 7-rano.biore sie za testy
    funkcjonalne

--- DjangoReadyProject/DjangoProject/src/CalendarApp/ReminderThread.py
+++ DjangoReadyProject/DjangoProject/src/CalendarApp/ReminderThread.py
@@ -1,7 +1,7 @@
 from threading import Thread
 from django.contrib.auth.models import User
 from GGLibrary.pygglib import GGSession
-from CalendarApp.models import *
+from datetime import datetime,timedelta
 import time
 
 class ReminderThread(Thread):
@@ -10,20 +10,31 @@ class ReminderThread(Thread):
     
     def run(self):
         try:
-            gg = GGSession(49651904, 'Master1234')
-            gg.login()
-            print 'Hello'
-            year, month, day = time.localtime()[:3]
-            for user in User.objects.all():
-                needtobereminded = user.entry_set.filter(remind=True,date__year=year,date__month=month,date__day=day)
-                if needtobereminded.count()!=0:
-                    print 'Uzytkownik {0} ma jakies eventy na dzisiaj'.format(user.username)
-                    uA = user.accounts
-                    userGG = uA.GGNumber
-                    gg.send_msg(int(userGG),'Hej. To wiadomosc z Django Calendar-a.\nMasz na dzisiaj zapisane pare Eventow w naszym Serwisie.\nOto one:')
-                    for event in needtobereminded:
-                        gg.send_msg(int(userGG),'Title: {0} \n Snippet: {1}\n Body: {2}\n'.format(event.title,event.snippet,event.body))
-                    gg.send_msg(int(userGG),'Do Uslyszenia - Zespol Django Calendar')
+            work = True
+            today = datetime.now()
+            while(work): 
+                today = today.replace(hour=7, minute=0,second=0, microsecond=0)
+                gg = GGSession(49651904, 'Master1234')
+                gg.login()
+                print 'Hello'
+                year, month, day = time.localtime()[:3]
+                for user in User.objects.all():
+                    needtobereminded = user.entry_set.filter(remind=True,date__year=year,date__month=month,date__day=day)
+                    if needtobereminded.count()!=0:
+                        print 'Uzytkownik {0} ma jakies eventy na dzisiaj'.format(user.username)
+                        uA = user.accounts
+                        userGG = uA.GGNumber
+                        gg.send_msg(int(userGG),'Hej. To wiadomosc z Django Calendar-a.\nMasz na dzisiaj zapisane pare Eventow w naszym Serwisie.\nOto one:')
+                        for event in needtobereminded:
+                            gg.send_msg(int(userGG),'Title: {0} \n Snippet: {1}\n Body: {2}\n'.format(event.title,event.snippet,event.body))
+                        gg.send_msg(int(userGG),'Do Uslyszenia - Zespol Django Calendar')
+                work = False
+                while(work == False):
+                    if datetime.now().replace(microsecond=0) == (today + timedelta(days=1)).replace(minute=0,second=0, microsecond=0):
+                        today = (today + timedelta(days=1))
+                        print 'Watek Rusza:'
+                        print today
+                        work = True   
             gg.logout()
         except Exception,e:
             print e.message     
